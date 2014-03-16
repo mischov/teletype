f517b4f9e58f2489066f528e947d7cd15d92ee26
Mischov <mmischov@gmail.com>
2014-03-15 23:51:52 -0500

    Annotated.

--- src-cljs/core.cljs
+++ src-cljs/core.cljs
@@ -6,11 +6,19 @@
 ;;  Defs  ;;
 ;;;;;;;;;;;;
 
-(def to-html js/marked)
+(def to-html
+  "Because cljsbuild prepends marked.js to compiled cljs,
+   can access via js/-whatever-"
+  js/marked)
 
 ;;;;;;;;;;;;;;;;;;;;;
 ;;  Util Functions ;;
 ;;;;;;;;;;;;;;;;;;;;;
+
+; These are all pretty self-explanatory, but window-height
+; and client-height wrap functions around access of those
+; respective properties, while set-full-height sets the
+; height of an element to a little less than full height.
 
 (defn window-height
   []
@@ -26,6 +34,9 @@
     (dommy/set-style! elem :height (str (- h 25) "px"))))
 
 (defn sync
+  "Allows two elements of possibly different height to
+   scroll together, so that the second element reaches
+   the bottom at the same time as the first."
   [e1 e2]
   (let [e1-scrollheight (.-scrollHeight e1)
         e2-scrollheight (.-scrollHeight e2)
@@ -39,6 +50,9 @@
 ;;;;;;;;;;;;
 
 (defn preview-md
+  "Reads the value of the textarea with the id 'markdown',
+   uses marked to convert that value into html, and sets
+   that html as the contents of the div with the id 'preview'."
   []
   (let [markdown (sel1 :#markdown)
         preview (sel1 :#preview)
@@ -47,6 +61,10 @@
     (dommy/set-html! preview content)))
 
 (defn init
+  "Attempts to preview any markdown initially provided,
+   sets the markdown and preview elements to full height,
+   and creates listeners to preview markdown and sync the
+   markdown textarea with the preview div."
   []
   (let [markdown (sel1 :#markdown)
         preview (sel1 :#preview)]
@@ -56,4 +74,5 @@
     (dommy/listen! markdown :keyup preview-md)
     (dommy/listen! markdown :scroll #(sync markdown preview))))
 
+; 'Ey 'O, let's go!
 (init)
