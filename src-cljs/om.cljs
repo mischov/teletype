24fb2b8f81d88861886245e3afa13ea6c30801ae
Mischov <mmischov@gmail.com>
2014-08-09 16:11:50 -0500

    Added simple localstore-based autosave to Om example.

--- src-cljs/om.cljs
+++ src-cljs/om.cljs
@@ -3,10 +3,17 @@
   (:require [om.core :as om :include-macros true]
             [sablono.core :as html :refer-macros [html]]
             [cljs.core.async :as async]
-            [goog.events :as events])
+            [goog.events :as events]
+            [teletype.store :as store]
+            [teletype.store.localstore :as localstore])
   (:import
    [goog.events EventType]
    [goog.async Throttle]))
+
+
+;;;;;;;;;;;;;;;;
+;;  Markdown  ;;
+;;;;;;;;;;;;;;;;
 
 
 (def to-html
@@ -55,15 +62,51 @@
     (events/listen markdown goog.events.EventType/SCROLL
                    #(.fire sync))))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Save and Autosave  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defn set-timeout
+  [timeout f]
+
+  (js/setTimeout f timeout))
+
+
+(defn clear-timeout
+  [d]
+
+  (js/clearTimeout d))
+
+
 ;;;;;;;;;;
 ;;  Om  ;;
 ;;;;;;;;;;
 
 
+(defn autosave
+  [owner time f]
+
+  (let [old-timeout (om/get-state owner :autosave)
+        new-timeout (set-timeout time f)]
+    (when old-timeout
+      (clear-timeout old-timeout))
+    (om/set-state! owner :timeout new-timeout)))
+
+
+(defn save-markdown
+  [owner markdown]
+
+  (let [localstore (om/get-state owner :localstore)]
+    (store/write-md localstore markdown)))
+
+
 (defn handle-change
   [e owner {:keys [markdown]}]
 
-  (om/set-state! owner :markdown (.. e -target -value)))
+  (om/set-state! owner :markdown (.. e -target -value))
+  (autosave owner 1000 #(save-markdown owner markdown)))
 
 
 (defn teletype-view
@@ -72,12 +115,19 @@
   (reify
     om/IInitState
     (init-state [_]
-      {:markdown ""})
+      {:markdown ""
+       :autosave nil
+       :localstore (localstore/create-store "test")})
 
     om/IDidMount
     (did-mount [_]
       (markdown-scroll (om/get-node owner "markdown")
-                       (om/get-node owner "preview")))
+                       (om/get-node owner "preview"))
+
+      (let [localstore (om/get-state owner :localstore)
+            md (store/read-md localstore)]
+        (when (seq md)
+          (om/set-state! owner :markdown md))))
 
     om/IRenderState
     (render-state [_ {:keys [markdown] :as state}]
